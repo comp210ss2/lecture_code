375d8a81973537168df2b5c079c0a9f24ab47b08
Jesse Wei <jesse@cs.unc.edu>
2024-06-28 08:30:31 -0400

    Nit NotProgrammingToInterface comment

--- src/main/java/comp210/L03/Interface/NotProgrammingToInterface.java
+++ src/main/java/comp210/L03/Interface/NotProgrammingToInterface.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class NotProgrammingToInterface {
     public static void main(String[] args) {
         // Key difference: type of l is now the concrete implementation LinkedList, not the interface List
-        // Try changing LinkedList<>() to ArrayList<>()
+        // Try changing LinkedList<>() (the type on left and the constructor on right) to ArrayList<>()
         LinkedList<Integer> l = new LinkedList<>();
         l.add(1);
         l.add(2);
