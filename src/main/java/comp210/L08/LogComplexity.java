62a869c92b06361652904872ab451af9ce730a46
Jesse Wei <jesse@cs.unc.edu>
2024-07-08 15:03:48 -0400

    Nit log complexity code

--- src/main/java/comp210/L08/LogComplexity.java
+++ src/main/java/comp210/L08/LogComplexity.java
@@ -3,7 +3,7 @@ package comp210.L08;
 public class LogComplexity {
   public static void main(String[] args) {
     int n = args.length;
-    for (int i = 0; i < n; i *= 2) {
+    for (int i = 1; i < n; i *= 2) {
       System.out.println(i);
     }
   }
