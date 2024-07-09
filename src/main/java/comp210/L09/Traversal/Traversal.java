0db007430ff2a49e05c70b1bb2ad391c6ffcad27
Jesse Wei <jesse@cs.unc.edu>
2024-07-09 13:05:16 -0400

    Nit traversal

--- src/main/java/comp210/L09/Traversal/Traversal.java
+++ src/main/java/comp210/L09/Traversal/Traversal.java
@@ -4,18 +4,48 @@ public class Traversal {
   public static void main(String[] args) {
     Node<String> root = createTree();
 
+    System.out.println("pre-order:");
     preOrder(root);
+
+    System.out.println("\npost-order:");
+    postOrder(root);
+
+    System.out.println("\nin-order:");
+    inOrder(root);
   }
 
   private static void preOrder(Node<String> root) {
     if (!(root == null)) {
-      System.out.println(root.getValue());
+      System.out.print(root.getValue() + " ");
       preOrder(root.getLeft());
       preOrder(root.getMiddle());
       preOrder(root.getRight());
     }
   }
 
+  private static void postOrder(Node<String> root) {
+    if (!(root == null)) {
+      postOrder(root.getLeft());
+      postOrder(root.getMiddle());
+      postOrder(root.getRight());
+      System.out.print(root.getValue() + " ");
+    }
+  }
+
+  private static void inOrder(Node<String> root) {
+    if (!(root == null)) {
+      // for arity 3, we arbitrarily decide to process L, then value, then M and R
+      // could also be L, M, value, then R
+      inOrder(root.getLeft());
+      System.out.print(root.getValue() + " ");
+      inOrder(root.getMiddle());
+      inOrder(root.getRight());
+    }
+  }
+
+  /**
+   * @return the tree of Strings used in L9 to explain traversals
+   */
   private static Node<String> createTree() {
     Node<String> root = new Node("lo");
 
