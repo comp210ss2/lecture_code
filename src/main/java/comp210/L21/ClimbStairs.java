1ebd12ef01fa3d9eac74e7c4c39b5b482132c124
Jesse Wei <jesse@cs.unc.edu>
2024-07-25 14:02:07 -0400

    Add ClimbStairs and HouseRobber templates

---
+++ src/main/java/comp210/L21/ClimbStairs.java
@@ -0,0 +1,38 @@
+package comp210.L21;
+
+// In dynamic programming, you solve each subproblem only once and store the results
+// This avoids redundant computations, leading to more efficient solutions
+// https://neetcode.io/problems/climbing-stairs
+// Can paste code there to run test cases
+public class ClimbStairs {
+    public static void main(String[] args) {
+        // Test here, if needed
+    }
+
+    public static int climbStairsNaive(int n) {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Use memoization array
+     *
+     * @param n
+     * @return
+     */
+    public static int climbStairsDP(int n) {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Use O(1) space
+     *
+     * @param n
+     * @return
+     */
+    public static int climbStairsDPConstantSpace(int n) {
+        // TODO
+        return 0;
+    }
+}
---
+++ src/main/java/comp210/L21/HouseRobber.java
@@ -0,0 +1,30 @@
+package comp210.L21;
+
+import java.util.Arrays;
+
+// https://neetcode.io/problems/house-robber
+public class HouseRobber {
+    public static void main(String[] args) {}
+
+    /**
+     * Use memo array (or skip to O(1) space below if you want)
+     *
+     * @param nums
+     * @return
+     */
+    public static int robArray(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * O(1) space
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob(int[] nums) {
+        // TODO
+        return 0;
+    }
+}
